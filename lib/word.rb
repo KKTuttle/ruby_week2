b44f7611ae310c62e286a5b90df7dd2f86d3c22b
Katarina Tuttle <katka.karolcikova@gmail.com>
2016-07-08 10:13:03 -0700

    Add passing spec for .definition

--- lib/word.rb
+++ lib/word.rb
@@ -12,11 +12,16 @@ class Word
   define_singleton_method(:all) do
     @@words
   end
+
   define_method(:save) do
     @@words.push(self)
   end
+
   define_singleton_method(:clear) do
     @@words = []
   end
 
+  # define_method(:definition) do
+  #   @definition.push(definition)
+  # end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,7 +1,7 @@
 require('rspec')
 require('pry')
 require('word')
-# require('definition')
+require('definition')
 require('launchy')
 
 describe(Word) do
@@ -39,11 +39,18 @@ describe(Word) do
     end
   end
 
-  # describe("") do
-  #   it "" do
+  describe('#definition') do
+    it "returns an epmty array of definitions for word" do
+      test_word = Word.new({:word => 'Stock'})
+      expect(test_word.definition()).to(eq([]))
+    end
+  end
+  # describe("#add_definition") do
+  #   it "adds a definition to the word" do
   #     test_word = Word.new({:word => 'Stock'})
-  #
-  #     expect().to(eq())
+  #     test_definition = Definition.new({:definition => "A stock is a type of security that signifies ownership in a corporation and represents a claim on part of the corporation's assets and earnings."})
+  #     test_word.add_definition(test_definition)
+  #     expect(test_word.definition).to(eq([test_definition]))
   #   end
   # end
 

7a0558e54a92642a0afecd0ae2a06efa8a4ac48f
Katarina Tuttle <katka.karolcikova@gmail.com>
2016-07-08 09:47:41 -0700

    Edit definition.rb and app.rb

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,2 +1,7 @@
-# class Definition
-#   attr_reader(:definition)
+class Definition
+  attr_reader(:definition)
+
+  define_method(:initialize) do |attributes|
+    @definition = attributes[:definition]
+  end
+end
